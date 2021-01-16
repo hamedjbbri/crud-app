6c54c03bd042175bda9b59d7b4b14496c9781179
Hamed Jabbari <jabbary.hamed@gmail.com>
2021-01-15 23:19:10 -0500

    ui and folder setup

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,14 +1,11 @@
 import express from 'express'
+import { getPosts, createPost } from '../controllers/posts.js'
 
 const router = express.Router();
 
-
-
-router.get('/', (req,res) => {
-    
-    res.send(`this works`);
-
-});
+ 
+router.get('/', getPosts);
+router.post('/', createPost);
 
 
 export default router
